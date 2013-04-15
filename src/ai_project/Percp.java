f86d3ba57f1c86771073f3939dce47c5a36673c1
Brian Lamb <brian.d.lamb@email.wsu.edu>
2013-04-14 20:31:39 -0700

    add test code, but still have not tested

--- src/ai_project/Percp.java
+++ src/ai_project/Percp.java
@@ -1,6 +1,7 @@
 package ai_project;
 
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 
 
@@ -117,6 +118,83 @@ public class Percp {
 				
 			}
 			alpha*=.01;
+		}
+		if(args.length>1){
+			java.io.File testD;
+			testD=new java.io.File(args[1]);
+			if(testD.isDirectory()){
+				List <String> testF=new ArrayList<String>();
+				String [] testFiles=testD.list();
+				for(String f:testFiles){
+					testD=new java.io.File(args[1]+"/"+f);
+					try {
+						String st;
+						Scanner s1=new Scanner(testD);
+						st=s1.nextLine();
+						while(s1.hasNextLine()){
+							st.concat(s1.nextLine());
+						}
+						testF.add(st);
+						s1.close();
+					} catch (FileNotFoundException e) {
+					}
+				}
+				PrintWriter p1=null;
+				try {
+					 p1=new PrintWriter("./data/results.txt");
+				} catch (FileNotFoundException e) {
+				}
+				Perceptron testP=new Perceptron(testF,"ts");
+				for(String f:testP.FilesToLearn){
+					String [] g=f.split(" ");
+					int totalCount=0;
+					for(String d:g){
+						for(wordRecord q:b){
+							if(d==q.word){
+								q.count+=1;
+							}
+						}
+						totalCount+=1;
+					}
+					for(wordRecord d:b){
+						d.countP=((float)d.countP)/totalCount;
+					}
+					boolean drV=myPerceptrons.get(0).vote(b);
+					boolean dtV=myPerceptrons.get(1).vote(b);
+					boolean lV=myPerceptrons.get(2).vote(b);
+					if(drV&&!dtV&!lV){
+						p1.println(f+" "+"dr");
+					}
+					else if(!drV&&dtV&!lV){
+						p1.println(f+" "+"dt");
+					}
+					else if(!drV&&!dtV&&lV){
+						p1.println(f+" "+"l");
+					}
+					else{
+						while(true){
+							int ranChoice=(int)(Math.random()*3.0);
+							if(ranChoice==1&&drV){
+								p1.println(f+" "+"dr");
+								break;
+							}
+							else if(ranChoice==2&&dtV){
+								p1.println(f+" "+"dt");
+								break;
+							}
+							else if(ranChoice==3&&lV){
+								p1.println(f+" "+"l");
+								break;
+							}
+						}
+					}
+					for(wordRecord j:b){
+						j.count=0;
+					}
+					
+				}
+				p1.close();
+			}
 		}
 	}
 
