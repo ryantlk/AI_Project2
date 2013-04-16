7c07abac22f0f06ae554fcddf3f4c55e0d9337da
ryantlk <ryantlk@ryantlk>
2013-04-15 18:30:03 -0700

    Added Naive_Bayes class.

---
+++ src/ai_project/Naive_Bayes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai_project;
+
+/**
+ *
+ * @author ryantlk
+ */
+public class Naive_Bayes {
+	String paths[];
+
+	public Naive_Bayes(String path1, String path2, String path3, String testPath) {
+		this.paths[0] = path1;
+		this.paths[1] = path2;
+		this.paths[2] = path3;
+		this.paths[3] = testPath;
+	}
+	
+	
+}
