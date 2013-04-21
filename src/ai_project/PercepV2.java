c4f901e566011b60b7fe52dd0cff2181de43c09d
Brian Lamb <brian.d.lamb@email.wsu.edu>
2013-04-21 10:10:07 -0700

    my improved version of perceptrons

---
+++ src/ai_project/PercepV2.java
@@ -0,0 +1,311 @@
+package ai_project;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+
+public class PercepV2 {
+	
+	public PercepV2(String learning,String testDir) {
+		List<String> Dr=new ArrayList<String>();
+		List<String> Dt=new ArrayList<String>();
+		List<String> L=new ArrayList<String>();
+		List<String>bagOfWords=new ArrayList<String>();
+		java.io.File h=new java.io.File(learning+"/DR");
+		wordRecord [] b;
+		String [] fileS=h.list();
+		for(String f:fileS){
+			h=new java.io.File(learning+"/DR/"+f);
+			try {
+				FileInputStream d=new FileInputStream(h);
+				byte [] ss;
+				ss=new byte[(int)h.length()];
+				try {
+					d.read(ss);
+					String st=new String(ss);
+					Dr.add(st);
+					d.close();
+				} catch (IOException e) {
+				}
+				
+			} catch (FileNotFoundException e) {
+			}
+		}
+		h=new java.io.File(learning+"/DT");
+		fileS=h.list();
+		for(String f:fileS){
+			h=new java.io.File(learning+"/DT/"+f);
+			try {
+				FileInputStream d=new FileInputStream(h);
+				byte [] ss;
+				ss=new byte[(int)h.length()];
+				try {
+					d.read(ss);
+					String st=new String(ss);
+					Dt.add(st);
+					d.close();
+				} catch (IOException e) {
+				}
+				
+			} catch (FileNotFoundException e) {
+			}
+		}
+		h=new java.io.File(learning+"/L");
+		fileS=h.list();
+		for(String f:fileS){
+			h=new java.io.File(learning+"/L/"+f);
+			try {
+				FileInputStream d=new FileInputStream(h);
+				byte [] ss;
+				ss=new byte[(int)h.length()];
+				try {
+					d.read(ss);
+					String st=new String(ss);
+					L.add(st);
+					d.close();
+				} catch (IOException e) {
+				}
+				
+			} catch (FileNotFoundException e) {
+			}
+		}
+		List<PerceptronV2> myPerceptrons=new ArrayList<PerceptronV2>();
+		PerceptronV2 drp=new PerceptronV2(Dr,"Dr"); 
+		myPerceptrons.add(drp);
+		PerceptronV2 dtp=new PerceptronV2(Dt,"Dt"); 
+		myPerceptrons.add(dtp);
+		PerceptronV2 lp=new PerceptronV2(L,"L"); 
+		myPerceptrons.add(lp);
+		for(PerceptronV2 f: myPerceptrons){
+			f.preprocess();
+		}
+		boolean toadd=true;
+		//construct the full list
+		for(PerceptronV2 f: myPerceptrons){
+			for(wordRecord z:f.bagOfWords2){
+				for(String g:bagOfWords){
+					if(g.equalsIgnoreCase(z.word)){
+						toadd=false;
+					}
+				}
+				if(toadd){
+					bagOfWords.add(z.word);
+				}
+				toadd=true;
+			}
+		}
+		for(PerceptronV2 f: myPerceptrons){
+			f.setupbag(bagOfWords);
+		}
+		for(String f:bagOfWords){
+			System.out.println(f);
+		}
+		b=new wordRecord[bagOfWords.size()];
+		for(int i=0;i<bagOfWords.size();i++){
+			b[i]=new wordRecord(bagOfWords.get(i));
+			b[i].count=0;
+		}
+		//to makes things simple we assume that there are equal number of files per class
+		//combine all strings into 1 combined list, in the following order
+		// dr,dt,l repeat
+		List <String>completeFiles=new ArrayList<String>();
+		int jjj=0;
+		int hhh=1;
+		for(PerceptronV2 f:myPerceptrons){
+			for(String z:f.FilesToLearn){
+				completeFiles.add(jjj, z+" "+f.classification);
+				jjj+=hhh;
+				if(jjj>completeFiles.size()){
+					jjj=completeFiles.size();
+				}
+			}
+			jjj=hhh;
+			hhh+=1;
+		}
+		double alpha=0.1;
+		for(int i=0;i<100;i++){
+				for(String s:completeFiles){
+					String [] g=s.split(" ");
+					int totalCount=0;
+					for(String d:g){
+						for(wordRecord q:b){
+							if(d.equalsIgnoreCase(q.word)){
+								q.count+=1;
+							}
+						}
+						totalCount+=1;
+					}
+					for(wordRecord d:b){
+						d.countP=((float)d.count)/totalCount;
+					}
+					myPerceptrons.get(0).train(b,g[g.length-1], alpha);
+					myPerceptrons.get(1).train(b,g[g.length-1], alpha);
+					myPerceptrons.get(2).train(b,g[g.length-1], alpha);
+					for(wordRecord j:b){
+						j.count=0;
+					}
+				}
+			alpha*=.99;
+		}
+		System.out.println("Percptron Trained");
+		if(testDir.length()>0){
+			java.io.File testD;
+			testD=new java.io.File(testDir);
+			List <String> theFiles=new ArrayList<String>();
+			if(testD.isDirectory()){
+				List <String> testF=new ArrayList<String>();
+				String [] testFiles=testD.list();
+				for(String f:testFiles){
+					testD=new java.io.File(testDir+"/"+f);
+					try {
+						FileInputStream d=new FileInputStream(testD);
+						byte [] ss;
+						ss=new byte[(int)testD.length()];
+						try {
+							d.read(ss);
+							String st=new String(ss);
+							testF.add(st);
+							theFiles.add(f);
+							d.close();
+						} catch (IOException e) {
+						}
+						
+					} catch (FileNotFoundException e) {
+					}
+				}
+				PrintWriter p1=null;
+				java.io.File kl=new java.io.File("./resultsV2.txt");
+				try {
+					kl.createNewFile();
+					 p1=new PrintWriter(kl);
+				} catch (FileNotFoundException e) {
+					System.out.println("no file");
+					System.exit(1);
+				} catch (IOException e) {
+					System.out.println("no file");
+					System.exit(1);
+				}
+				int fileN=0;
+				Perceptron testP=new Perceptron(testF,"ts");
+				for(String f:testP.FilesToLearn){
+					f=f.toLowerCase();
+					f=f.replaceAll("[^a-zA-Z]", " ");
+					f=f.replace("\n"," ");
+					String regex = "\\s{2,}"; 
+					f = f.replaceAll(regex, " "); 
+					f=f.trim();
+					String [] g=f.split(" ");
+					int totalCount=0;
+					for(String d:g){
+						for(wordRecord q:b){
+							if(d.equalsIgnoreCase(q.word)){
+								q.count+=1;
+								break;
+							}
+						}
+						totalCount+=1;
+					}
+					for(wordRecord d:b){
+						d.countP=((float)d.count)/totalCount;
+					}
+					boolean drV=myPerceptrons.get(0).vote(b);
+					boolean dtV=myPerceptrons.get(1).vote(b);
+					boolean lV=myPerceptrons.get(2).vote(b);
+					if(drV&&!dtV&&!lV){
+						p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+					}
+					else if(!drV&&dtV&&!lV){
+						p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+					}
+					else if(!drV&&!dtV&&lV){
+						p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+					}
+					else{
+						int ranChoice=(int)(Math.random()*3.0);
+						if(ranChoice==1){
+							if(drV){
+								p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+							}
+							else{
+								ranChoice=(int)(Math.random()*2);
+								if(ranChoice==1){
+									if(dtV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+									}
+								}
+								else{
+									if(lV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+									}
+								}
+							}
+						}
+						else if(ranChoice==2){
+							if(dtV){
+								p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+							}
+							else{
+								ranChoice=(int)(Math.random()*2);
+								if(ranChoice==1){
+									if(drV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+									}
+								}
+								else{
+									if(lV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+									}
+								}
+							}
+						}
+						else{
+							if(lV){
+								p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"l");
+							}
+							else{
+								ranChoice=(int)(Math.random()*2);
+								if(ranChoice==1){
+									if(dtV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+									}
+								}
+								else{
+									if(drV){
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dr");
+									}
+									else{
+										p1.println("Percptron v2 "+theFiles.get(fileN)+" "+"dt");
+									}
+								}
+							}
+						}
+					}
+					for(wordRecord j:b){
+						j.count=0;
+					}
+					fileN++;	
+				}
+				p1.close();
+			}
+		}
+	}
+
+}
---
+++ src/ai_project/PerceptronV2.java
@@ -0,0 +1,173 @@
+package ai_project;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+
+
+public class PerceptronV2 {
+		List<String> FilesToLearn;
+		List <wordRecord> bagOfWords2;
+		String [] myBag;
+		int total=0;
+		double [] weights;
+		String classification;
+		String [] CommonStrings={"the","be","to","of","and","a","in","that","have",
+				"i","it","for","not","on","with","he","as","you","do","at","this",
+				"but","his","by","from","they","we","say","her","she","or","an",
+				"will","my","one","all","would","there","their","what","so","up",
+				"out","if","about","who","get","which","go","me","when","make",
+				"can","like","time","no","just","him","know","take","people",
+				"into","year","your","good","some","could","them","see","other",
+				"than","then","now","look","only","come","its","over","think",
+				"also","back","after","use","two","how","our","work","first",
+				"well","way","even","new","want","because","any","these","give"
+				,"day","most","us"};//,"oregon","washington","said","under","hereby","person","shall",
+				//"such","may","are","number","date",",above","county","state","law","due",
+				//"division","department","tax","above","named","amount","following","was","social"};
+				//,"office","health","section","name","clerk","description","box","national","lance","benton"
+				//,"legal","xxx","inc","public","before","taxpayer","page","known","pay","company","personally"
+				//,"official","present","acknowledged","described","assessment","corporation","wessell"};
+		
+		public PerceptronV2(List<String> f,String s){
+			FilesToLearn=new ArrayList<String>();
+			for(String z: f){
+				FilesToLearn.add(z);
+			}
+			classification=s;
+		}
+		
+		public void preprocess(){
+			//first step do normal preprocessing
+			List<String> files2=new ArrayList<String>();
+			for(String f:FilesToLearn){
+				//conver all non alpha,non space chars to spaces
+				f=f.toLowerCase();
+				f=f.replaceAll("[^a-zA-Z]", " ");
+				f=f.replace("\n"," ");
+				String regex = "\\s{2,}"; 
+				f = f.replaceAll(regex, " "); 
+				f=f.trim();
+				files2.add(f);
+			}
+			FilesToLearn.clear();
+			for(String f: files2){
+				FilesToLearn.add(f);
+			}
+			//get word counts
+			boolean toadd=true;
+			List<wordRecord> bagOfWords=new ArrayList<wordRecord>();
+			for(String f:FilesToLearn){
+				String [] g=f.split(" ");
+				for(String hh:g){
+					for(wordRecord z:bagOfWords){
+						if(z.word.equals(hh)){
+							toadd=false;
+							z.count++;
+							break;
+						}
+					}
+					
+					if(toadd){
+						bagOfWords.add(new wordRecord(hh));
+					}
+					toadd=true;
+				}
+			}
+			//calculate total words
+			int totalCount=0;
+			for(wordRecord f:bagOfWords){
+				totalCount+=f.count;
+				//System.out.println(f.word);
+			}
+			//System.exit(1);
+			total=totalCount;
+			//get p(w/c)
+			for(wordRecord f:bagOfWords){
+				f.countP=((float)f.count)/((float)totalCount);
+			}
+			//this is kinda ugly but i am putting the word records into an array and is sorting by p(w/c)
+			Object [] p=bagOfWords.toArray();
+			wordRecord [] pp=new wordRecord[p.length];
+			for(int i=0;i<p.length;i++){
+				pp[i]=(wordRecord)p[i];
+			}
+			Arrays.sort(pp,new Comparator<Object>(){ public int compare(Object o1,Object o2){
+						wordRecord w1=(wordRecord)o1; wordRecord w2=(wordRecord) o2;
+								if(w1.countP<w2.countP)return 1; else if(w1.countP>w2.countP) return -1;
+								else return w1.word.compareTo(w2.word);};});
+			//grab to 20 words, or as many as we can
+			int wordC=0;
+			List<String> stringList=new ArrayList<String>();
+			for(wordRecord f:pp){
+				stringList.add(f.word);
+			}
+			//for(String f:CommonStrings){
+				//stringList.remove(f);
+			//}
+			List <String> toremove=new ArrayList<String>();
+			for(String f:stringList){
+				if(f.length()<3){
+					toremove.add(f);
+				}
+			}
+			for(String f:toremove){
+				stringList.remove(f);
+			}
+			bagOfWords2=new ArrayList<wordRecord>();
+			bagOfWords2.add(new wordRecord("lien"));
+			bagOfWords2.add(new wordRecord("trust"));
+			bagOfWords2.add(new wordRecord("reconveyance"));
+			//stringList.remove("lien");
+			//stringList.remove("trust");
+			//stringList.remove("reconveyance");
+			for(String f:stringList){
+				bagOfWords2.add(new wordRecord(f));
+				wordC++;
+				if(wordC==20)
+					break;
+			}
+		
+		}
+		
+		public void setupbag(List<String> bag){
+			myBag=new String[bag.size()];
+			weights=new double[bag.size()];
+			for(int i=0;i<myBag.length;i++){
+				myBag[i]=bag.get(i);
+				weights[i]=0.6;
+			}
+		}
+		
+		public void train(wordRecord[] wordList, String c, double alpha){
+			boolean results=vote(wordList);
+			int error=0;
+			if(results&&(!c.equalsIgnoreCase(classification))){
+				error=-1;	
+			}
+			if(!results&&(c.equalsIgnoreCase(classification))){
+				error=1;
+			}
+			if(error!=0){
+				for(int i=0;i<weights.length;i++){
+					weights[i]=weights[i]+alpha*error*wordList[i].countP;
+				}
+			}
+		}
+		
+		public boolean vote(wordRecord [] wordList){
+			int sum=0;
+			for(int i=0;i<wordList.length;i++){
+				sum+=weights[i]*((double)wordList[i].countP);
+			}
+			sum+=1;
+			if(sum>0){
+				return true;
+			}
+			return false;
+		}
+		
+}
